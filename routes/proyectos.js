1d2bbeff26a1d241e521e43e71831cc09cb8b75e
Jesús Rojas <jesusrojas.frontend@gmail.com>
2019-09-02 19:42:12 +0000

    Se agregaron las rutas de los proyectos

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -38,6 +38,88 @@ function proyectosApi(app){
             next(err) //esta es la forma en como llamamos el error
         }
     })
+    
+    /* @brief cuando utilizamos el metodo get del id del proyecto (/api/proyectos/id)
+     * nos va a devolver el proyecto por id
+     * @params req objeto request
+     * @params res objeto response
+     * @params next un callback
+    */
+    router.get('/:proyectoId', async function(req,res,next){
+        try{ /*Como es codigo asincrono es muy importante hacer el try catch */
+            const proyectos = await Promise.resolve(proyectosMock[0]) //en ves de devolver la coleccion devuelve el primer proyecto
+            
+            
+            //definimos el estatus y la respuesta json
+            res.status(200).json({
+                data: proyectos,
+                message: 'proyecto retrieved'
+            })
+        } catch(err){ //recivimos el error
+            next(err) //esta es la forma en como llamamos el error
+        }
+    })
+    
+    /* @brief cuando utilizamos el metodo post en el home (/api/proyectos)
+     * nos va a añadir un proyecto
+     * @params req objeto request
+     * @params res objeto response
+     * @params next un callback
+    */
+    router.post('/', async function(req,res,next){
+        try{ /*Como es codigo asincrono es muy importante hacer el try catch */
+       
+            const createProyectoId = await Promise.resolve(proyectosMock[0].id) // por ahora devuelve el id de la pelicula
+            
+            //definimos el estatus y la respuesta json
+            res.status(201).json({
+                data: createProyectoId,
+                message: 'proyecto create'
+            })
+        } catch(err){ //recivimos el error
+            next(err) //esta es la forma en como llamamos el error
+        }
+    })
+    
+    /* @brief cuando utilizamos el metodo put del proyecto (/api/proyectos/:id)
+     * nos va a actualizar el proyecto
+     * @params req objeto request
+     * @params res objeto response
+     * @params next un callback
+    */
+    router.put('/:proyectosId', async function(req,res,next){
+        try{ /*Como es codigo asincrono es muy importante hacer el try catch */
+            const updateProyectoId= await Promise.resolve(proyectosMock[0].id) //devuelve el id que actualizo
+            
+            //definimos el estatus y la respuesta json
+            res.status(200).json({
+                data: updateProyectoId,
+                message: 'proyectos updated'
+            })
+        } catch(err){ //recivimos el error
+            next(err) //esta es la forma en como llamamos el error
+        }
+    })
+    
+    /* @brief cuando utilizamos el metodo delete del proyecto (/api/proyectos/:id)
+     * nos va a borrar el proyecto
+     * @params req objeto request
+     * @params res objeto response
+     * @params next un callback
+    */
+    router.delete('/:proyectosId', async function(req,res,next){
+        try{ /*Como es codigo asincrono es muy importante hacer el try catch */
+            const deleteProyectoId= await Promise.resolve(proyectosMock[0].id) //devuelve el id que actualizo
+            
+            //definimos el estatus y la respuesta json
+            res.status(200).json({
+                data: deleteProyectoId,
+                message: 'proyectos updated'
+            })
+        } catch(err){ //recivimos el error
+            next(err) //esta es la forma en como llamamos el error
+        }
+    })
 }
 
 module.exports = proyectosApi;
